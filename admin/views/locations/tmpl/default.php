62d0ac3775650514ed903b5f0113da6b17bdb917
Florian Häusler <florian@didldu.com>
2017-03-08 20:08:52 +0100

    view tmpl added

---
+++ admin/views/locations/tmpl/default.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @version    1-1-0-0 // Y-m-d 2017-03-08
+ * @author     HR IT-Solutions Florian Häusler https://www.hr-it-solutions.com
+ * @copyright  Copyright (C) 2011 - 2017 Didldu e.K. | HR IT-Solutions
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+?>
+
+<div class="row-fluid">
+	<?php if (!empty($this->sidebar)) : ?>
+	<div id="j-sidebar-container" class="span2">
+	<?php echo $this->sidebar; ?>
+	</div>
+	<div id="j-main-container" class="span10">
+	<?php else : ?>
+	<div id="j-main-container" class="span12">
+	<?php endif; ?>
+		<div class="row-fluid">
+            
+            <!-- Component Description -->
+            <div class="text-center">
+                <p><?php echo JText::_('COM_DD_GMAPS_LOCATIONS_XML_DESCRIPTION'); ?></p>
+            </div>
+
+            <!-- Component Version Info -->
+            <div class="alert text-center">
+                <?php echo JText::sprintf('COM_DD_GMAPS_LOCATIONS_VERSION',
+                    DD_GMaps_LocationsHelper::getComponentVersion());
+                ?>
+            </div>
+
+            <hr>
+            <!-- Component Credits -->
+            <div class="text-center">
+                <p><small><?php echo nl2br(JText::sprintf('COM_DD_GMAPS_LOCATIONS_CREDITS',
+                        DD_GMaps_LocationsHelper::getComponentCoyright()));
+                ?></small></p>
+            </div>
+		</div>
+	</div>
+</div>
