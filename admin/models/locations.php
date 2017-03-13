23f150ca7eca5726001e9931659c7c1acfe46849
Florian Häusler <florian@didldu.com>
2017-03-13 12:49:23 +0100

    Improved code style phpcs Joomla Commented our Ordering (Ordering did not work currently)

--- admin/models/locations.php
+++ admin/models/locations.php
@@ -8,33 +8,42 @@
 
 defined('_JEXEC') or die;
 
+/**
+ * Class DD_GMaps_LocationsModelLocations
+ *
+ * @since  Version 1.1.0.0
+ */
 class DD_GMaps_LocationsModelLocations extends JModelList
 {
 
 	/**
+	 * getListQuery
+	 *
 	 * @return mixed
-	 * @since Version 3.6.5
+	 *
+	 * @since Version 1.1.0.0
 	 */
 	protected function getListQuery()
 	{
-
 		$db		= $this->getDbo();
 		$query	= $db->getQuery(true);
 
-		$query->select($this->getState(
-			'list.select', 'a.id, a.title, a.catid, a.state,' .
+		$query->select(
+			$this->getState('list.select', 'a.id, a.title, a.catid, a.state,' .
 			'a.company, a.street, a.location, a.zip, a.country, a.federalstate,' .
 			'a.publish_up, a.publish_down, a.ordering')
 		);
 
-		$query->from($db->quoteName('#__dd_gmaps_locations'),'a');
+		$query->from($db->quoteName('#__dd_gmaps_locations') . 'a');
 
 		// Filter by state
 		$published = $this->getState('filter.state');
+
 		if (is_numeric($published))
 		{
-			$query->where('a.state = '.(int) $published);
-		} elseif ($published === '')
+			$query->where('a.state = ' . (int) $published);
+		}
+		elseif ($published === '')
 		{
 			$query->where('(a.state IN (0, 1))');
 		}
@@ -45,29 +54,33 @@ class DD_GMaps_LocationsModelLocations extends JModelList
 
 		// Filter by search
 		$search = $this->getState('filter.search');
+
 		if (!empty($search))
 		{
 			if (stripos($search, 'id:') === 0)
 			{
-				$query->where('a.id = '.(int) substr($search, 3));
-			} else {
-				$search = $db->Quote('%'.$db->escape($search, true).'%');
-				$query->where('(a.title LIKE '.$search.' OR a.company LIKE '.$search.
-					' OR a.street LIKE '.$search.' OR a.location LIKE '.$search.
-					' OR a.zip LIKE '.$search.' OR a.country LIKE '.$search.
-					' OR a.federalstate LIKE '.$search.')');
+				$query->where('a.id = ' . (int) substr($search, 3));
+			}
+			else
+			{
+				$search = $db->Quote('%' . $db->escape($search, true) . '%');
+				$query->where('(a.title LIKE ' . $search . ' OR a.company LIKE ' . $search .
+					' OR a.street LIKE ' . $search . ' OR a.location LIKE ' . $search .
+					' OR a.zip LIKE ' . $search . ' OR a.country LIKE ' . $search .
+					' OR a.federalstate LIKE ' . $search . ')');
 			}
 		}
 
 		// Ordering
-		$orderCol = $this->state->get('list.ordering');
+		/*$orderCol = $this->state->get('list.ordering');
 		$orderDir = $this->state->get('list.direction');
+
 		if ($orderCol == 'a.ordering')
 		{
-			$orderCol = 'c.title '.$orderDir.', a.ordering';
+			$orderCol = 'c.title ' . $orderDir . ', a.ordering';
 		}
-		$query->order($db->escape($orderCol.' '.$orderDir));
 
+		$query->order($db->escape($orderCol . ' ' . $orderDir));*/
 
 		return $query;
 	}
