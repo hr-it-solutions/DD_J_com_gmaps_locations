fabf42d084eef4930d7dce84a8622448b3d04a84
Florian Häusler <florian@didldu.com>
2017-03-13 14:20:58 +0100

    added locations view tmpl added adminForm

--- admin/views/locations/tmpl/default.php
+++ admin/views/locations/tmpl/default.php
@@ -7,24 +7,110 @@
  **/
 
 defined('_JEXEC') or die;
+
+$listOrder = '';
+$listDirn = '';
+
 ?>
+<div id="dd_gmaps_locations-locations" class="row-fluid dd_gmaps_locations">
+    <form action="<?php echo JRoute::_('index.php?option=com_dd_gmaps_locations&view=locations'); ?>" method="post" name="adminForm" id="adminForm">
+        <?php if (!empty($this->sidebar)):?>
+        <div id="j-sidebar-container" class="span2">
+        <?php echo $this->sidebar; ?>
+        </div>
+        <div id="j-main-container" class="span10">
+        <?php else: ?>
+        <div id="j-main-container" class="span12">
+        <?php endif; ?>
+            <div class="clearfix"></div>
+            <table class="table table-striped" id="dd_gmaps_locationsList">
+                <thead>
+                    <th width="1%" class="center hidden-phone">
+                        <input type="checkbox" name="check-toggle" value=""
+                               title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>"
+                               onclick="Joomla.checkAll(this)" />
+                    </th>
+                    <th class="nowrap title">
+                        <?php echo JHtml::_('grid.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
+                    </th>
+                    <th class="nowrap center">
+	                    <?php echo JHtml::_('grid.sort', 'COM_DD_GMAPS_LOCATIONS_HEADING_COMPANY', 'a.company', $listDirn, $listOrder); ?>
+                    </th>
+                    <th class="nowrap hidden-phone">
+	                    <?php echo JHtml::_('grid.sort', 'COM_DD_GMAPS_LOCATIONS_HEADING_CONTACTPERSON', 'a.contact_person', $listDirn, $listOrder); ?>
+                    </th>
+                    <th class="nowrap hidden-phone">
+	                    <?php echo JHtml::_('grid.sort', 'COM_DD_GMAPS_LOCATIONS_HEADING_PHONE', 'a.phone', $listDirn, $listOrder); ?>
+                    </th>
+                    <th class="nowrap">
+	                    <?php echo JHtml::_('grid.sort', 'COM_DD_GMAPS_LOCATIONS_HEADING_STREET', 'a.street', $listDirn, $listOrder); ?>
+                    </th>
+                    <th class="nowrap">
+	                    <?php echo JHtml::_('grid.sort', 'COM_DD_GMAPS_LOCATIONS_HEADING_LOCATION', 'a.location', $listDirn, $listOrder); ?>
+                    </th>
+                    <th class="nowrap">
+	                    <?php echo JHtml::_('grid.sort', 'COM_DD_GMAPS_LOCATIONS_HEADING_ZIP', 'a.zip', $listDirn, $listOrder); ?>
+                    </th>
+                    <th class="nowrap">
+	                    <?php echo JHtml::_('grid.sort', 'COM_DD_GMAPS_LOCATIONS_HEADING_COUNTRY', 'a.country', $listDirn, $listOrder); ?>
+                    </th>
+                    <th class="nowrap hidden-phone">
+	                    <?php echo JHtml::_('grid.sort', 'JCATEGORY', 'c.category_title', $listDirn, $listOrder); ?>
+                    </th>
+                </thead>
+                <tbody>
+                <?php foreach ($this->items as $i => $item): ?>
+                    <tr class="row<?php echo $i % 2; ?>">
+                        <td class="center hidden-phone">
+                            <?php echo JHtml::_('grid.id', $i, $item->id); ?>
+                        </td>
+                        <td class="nowrap has-context">
+                            <a href="<?php echo JRoute::_('index.php?option=com_dd_gmaps_locations&task=location.edit&id=' . (int) $item->id);?> ">
+                                <?php echo $this->escape($item->title);?>
+                            </a>
+                        </td>
+                        <td class="nowrap center">
+	                        <?php echo $this->escape($item->company); ?>
+                        </td>
+                        <td class="nowrap hidden-phone">
+		                    <?php echo $this->escape($item->contact_person); ?>
+                        </td>
+                        <td class="nowrap hidden-phone">
+		                    <?php echo $this->escape($item->phone); ?>
+                        </td>
+                        <td class="nowrap">
+		                    <?php echo $this->escape($item->street); ?>
+                        </td>
+                        <td class="nowrap">
+		                    <?php echo $this->escape($item->location); ?>
+                        </td>
+                        <td class="nowrap">
+		                    <?php echo $this->escape($item->zip); ?>
+                        </td>
+                        <td class="nowrap">
+		                    <?php echo JText::_($this->escape($item->country)); ?>
+                        </td>
+                        <td class="nowrap hidden-phone">
+		                    <?php echo $this->escape($item->category_title); ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
 
-<div class="row-fluid">
-	<?php if (!empty($this->sidebar)) : ?>
-	<div id="j-sidebar-container" class="span2">
-	<?php echo $this->sidebar; ?>
-	</div>
-	<div id="j-main-container" class="span10">
-	<?php else : ?>
-	<div id="j-main-container" class="span12">
-	<?php endif; ?>
-		<div class="row-fluid">
+            <input type="hidden" name="task" value="" />
+            <input type="hidden" name="boxckecked" value="0" />
+            <input type="hidden" name="filter_order" value="<?php echo $listDirn; ?>">
+            <input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>">
 
-            <hr>
-            <!-- Component Credits -->
-            <div class="text-center">
-                <p><small><?php echo nl2br(JText::sprintf('COM_DD_GMAPS_LOCATIONS_CREDITS', DD_GMaps_LocationsHelper::getComponentCoyright())); ?></small></p>
+            <div class="row-fluid">
+
+                <hr>
+                <!-- Component Credits -->
+                <div class="text-center">
+                    <p><small><?php echo nl2br(JText::sprintf('COM_DD_GMAPS_LOCATIONS_CREDITS', DD_GMaps_LocationsHelper::getComponentCoyright())); ?></small></p>
+                </div>
             </div>
-		</div>
-	</div>
+        </div>
+    </form>
 </div>
