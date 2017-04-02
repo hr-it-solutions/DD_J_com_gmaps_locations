6c293e211f527b83de5c735b58af2c8b54b17170
Florian Häusler <florian@didldu.com>
2017-04-02 20:32:39 +0200

    Restore fork Version Update

--- admin/helpers/dd_gmaps_locations.php
+++ admin/helpers/dd_gmaps_locations.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version    1-1-0-0 // Y-m-d 2017-03-07
+ * @version    1-1-0-1 // Y-m-d 2017-04-02
  * @author     HR IT-Solutions Florian Häusler https://www.hr-it-solutions.com
  * @copyright  Copyright (C) 2011 - 2017 Didldu e.K. | HR IT-Solutions
  * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
