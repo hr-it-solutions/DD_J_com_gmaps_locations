d63e4b8b2c1b88c99f8ec351d1b7da823060f32e
Florian Häusler <florian@didldu.com>
2017-04-29 01:18:55 +0200

    New legal notes / copyright header

--- media/js/dd_gmaps_locations.js
+++ media/js/dd_gmaps_locations.js
@@ -1,6 +1,7 @@
 /**
- * @version    1-1-0-0 // Y-m-d 2017-02-25
- * @author     HR IT-Solutions Florian Häusler https://www.hr-it-solutions.com
+ * @package    DD_GMaps_Locations
+ *
+ * @author     HR IT-Solutions Florian Häusler <info@hr-it-solutions.com>
  * @copyright  Copyright (C) 2011 - 2017 Didldu e.K. | HR IT-Solutions
  * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
  **/
