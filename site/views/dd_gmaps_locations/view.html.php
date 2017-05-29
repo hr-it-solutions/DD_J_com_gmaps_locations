57a755294f93da164fc6c2a3577bf4fd513eba0d
Florian Häusler <florian@didldu.com>
2017-05-29 19:07:49 +0200

    Instructions and documentation view issue #30

---
+++ site/views/dd_gmaps_locations/view.html.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package    DD_GMaps_Locations
+ *
+ * @author     HR IT-Solutions Florian Häusler <info@hr-it-solutions.com>
+ * @copyright  Copyright (C) 2011 - 2017 Didldu e.K. | HR IT-Solutions
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+class DD_GMaps_LocationsViewDD_GMaps_Locations extends JViewLegacy
+{
+
+}
