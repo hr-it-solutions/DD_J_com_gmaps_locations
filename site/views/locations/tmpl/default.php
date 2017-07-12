e2cb8765ba0bf128f47cc5ec0a41388438422a44
Florian Häusler <florian@didldu.com>
2017-07-13 01:53:23 +0200

    fixed #32 On locations view notice should be displayed if no location was defnied Revision

--- site/views/locations/tmpl/default.php
+++ site/views/locations/tmpl/default.php
@@ -11,38 +11,44 @@ defined('_JEXEC') or die;
 
 ?>
 <div class="dd_gmaps_locations locations well">
-    <div class="row-fluid">
-    <?php
-foreach ($this->items as $i => $item): ?>
-<?php if ($i % 2 == 0): ?>
+<div class="row-fluid">
+<?php if (empty($this->items)): ?>
+    <div class="alert alert-info center">
+       <?php echo JText::_('COM_DD_GMAPS_LOCATIONS_NO_RESULT'); ?>
     </div>
-    <div class="row-fluid">
+<?php endif; ?>
+<?php
+foreach ($this->items as $i => $item):
+    ?>
+<?php if ($i % 2 == 0): ?>
+</div>
+<div class="row-fluid">
 <?php endif; ?>
         <?php include JPATH_COMPONENT . '/views/locations/tmpl/default_items.php'; ?>
 <?php
 endforeach;?>
-        <div id="InserBefore" class="clear"></div>
-        <div class="load-more">
-            <button id="load-more" class="btn"><?php echo JText::_('COM_DD_GMAPS_LOCATIONS_LOAD_MORE'); ?></button>
-        </div>
-
-        <script language="javascript" type="text/javascript">
-
-            var start = <?php echo (int) $this->params->get('items_to_list', 6); ?>,
-                limit = <?php echo (int) $this->params->get('items_more', 4); ?>,
-                geolocate = '<?php echo $this->app->input->get('geolocate', '', 'STRING'); ?>',
-                locationLatLng = '<?php echo $this->app->input->get('locationLatLng', 0, 'STRING'); ?>',
-                fulltext_search = '<?php echo $this->app->input->get('fulltext_search', '', 'STRING'); ?>',
-                category_filter = '<?php echo $this->app->input->get('category_filter', '', 'STRING'); ?>',
-                federalstate_filter = '<?php echo $this->app->input->get('federalstate_filter', '', 'STRING'); ?>',
-                activeAlias = '<?php echo $this->active_alias; ?>';
-
-            jQuery('#load-more').click(function () {
-                processAjax();
-            });
-
-            checkNextItems();
-        </script>
-
+    <div id="InserBefore" class="clear"></div>
+    <div class="load-more">
+        <button id="load-more" class="btn"><?php echo JText::_('COM_DD_GMAPS_LOCATIONS_LOAD_MORE'); ?></button>
     </div>
+
+    <script language="javascript" type="text/javascript">
+
+        var start = <?php echo (int) $this->params->get('items_to_list', 6); ?>,
+            limit = <?php echo (int) $this->params->get('items_more', 4); ?>,
+            geolocate = '<?php echo $this->app->input->get('geolocate', '', 'STRING'); ?>',
+            locationLatLng = '<?php echo $this->app->input->get('locationLatLng', 0, 'STRING'); ?>',
+            fulltext_search = '<?php echo $this->app->input->get('fulltext_search', '', 'STRING'); ?>',
+            category_filter = '<?php echo $this->app->input->get('category_filter', '', 'STRING'); ?>',
+            federalstate_filter = '<?php echo $this->app->input->get('federalstate_filter', '', 'STRING'); ?>',
+            activeAlias = '<?php echo $this->active_alias; ?>';
+
+        jQuery('#load-more').click(function () {
+            processAjax();
+        });
+
+        checkNextItems();
+    </script>
+
+</div>
 </div>
