7e3d29ece2aecd465a2b605b09a63d1499d3d9b2
Florian Häusler <florian@didldu.com>
2018-06-08 12:58:14 +0200

    publish_up publish_down

--- site/models/locations.php
+++ site/models/locations.php
@@ -204,17 +204,29 @@ class DD_GMaps_LocationsModelLocations extends JModelList {
 		if ($this->getState('filter.state') !== 'all')
 		{
 			$query->where('a.state = 1');
+
+			$query->where('('.
+				$db->qn('a.publish_up') . ' = ' . $db->q('0000-00-00 00:00:00') .
+				' OR ' .
+				$db->qn('a.publish_up') . '<= ' . $db->q(JFactory::getDate()->format('Y-m-d H:i:s'))
+				. ')');
+
+			$query->where('('.
+				$db->qn('a.publish_down') . ' = ' . $db->q('0000-00-00 00:00:00') .
+				' OR ' .
+				$db->qn('a.publish_down') . '>= ' . $db->q(JFactory::getDate()->format('Y-m-d H:i:s'))
+				. ')');
 		}
 
 		if (isset($filterInput->fulltext_search))
 		{
-			$query->where(
+			$query->where('(' .
 				$db->qn('a.title') . ' LIKE "%' . $filterInput->fulltext_search . '%" OR ' .
 				$db->qn('a.company') . ' LIKE "%' . $filterInput->fulltext_search . '%" OR ' .
 				$db->qn('a.contact_person') . ' LIKE "%' . $filterInput->fulltext_search . '%" OR ' .
 				$db->qn('a.short_description') . ' LIKE "%' . $filterInput->fulltext_search . '%" OR ' .
 				$db->qn('a.description') . ' LIKE "%' . $filterInput->fulltext_search . '%" OR ' .
-				$db->qn('c.title') . ' LIKE "%' . $filterInput->fulltext_search . '%"');
+				$db->qn('c.title') . ' LIKE "%' . $filterInput->fulltext_search . '%"' . ')');
 		}
 
 		// Filter by author
